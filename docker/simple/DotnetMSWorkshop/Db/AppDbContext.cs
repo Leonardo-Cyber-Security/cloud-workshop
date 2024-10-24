93afcb8a6516311746f799ba66a4956c4933b3c2
Placido Papotto <placido.papotto@leonardo.com>
2024-10-24 22:38:19 +0200

    Fix mapping entity

--- docker/simple/DotnetMSWorkshop/Db/AppDbContext.cs
+++ docker/simple/DotnetMSWorkshop/Db/AppDbContext.cs
@@ -14,8 +14,20 @@ namespace DotnetMSWorkshop.Db
             modelBuilder.Entity<Item>()
                 .ToTable("items"); // Mappa la classe alla tabella Items
             modelBuilder.Entity<Item>()
+                .Property(e => e.Id)
+                .HasColumnName("id");
+                .ToTable("items");
+            modelBuilder.Entity<Item>()
                 .Property(e => e.Name)
                 .HasColumnName("name");
+                .ToTable("items");
+            modelBuilder.Entity<Item>()
+                .Property(e => e.Description)
+                .HasColumnName("description");
+                .ToTable("items");
+            modelBuilder.Entity<Item>()
+                .Property(e => e.Quantity)
+                .HasColumnName("quantity");
         }
     }
 }
